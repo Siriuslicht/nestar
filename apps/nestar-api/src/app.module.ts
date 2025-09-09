bd0a630cdb43b796145d5ac1515659c9c91dadf4
Siriuslicht <laurairlean@gmail.com>
2025-09-10 00:57:14 +0500

    feat: build project standards

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,6 +5,8 @@ import {ConfigModule} from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver } from "@nestjs/apollo";
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from "./components/components.module";
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
@@ -12,8 +14,11 @@ import { AppResolver } from './app.resolver';
     playground: true,
     uploads: false,
     autoSchemaFile: true,
-  })],
-  controllers: [AppController],
+  }),
+   ComponentsModule,
+   DatabaseModule,
+  ],
+  controllers: [AppController], 
   providers: [AppService, AppResolver],
 })
 export class AppModule {}
---
+++ apps/nestar-api/src/components/components.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MemberModule } from './member/member.module';
+import { PropertyModule } from './property/property.module';
+
+@Module({
+   imports: [MemberModule, PropertyModule]
+})
+
+export class ComponentsModule {}

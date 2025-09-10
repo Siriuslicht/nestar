510b7de74e84d27ea07db67aa5fa6cc37cb222fe
Siriuslicht <laurairlean@gmail.com>
2025-09-10 23:38:20 +0500

    feat: MITASK-ZJ

--- train.ts
+++ train.ts
@@ -1,1 +1,31 @@
 console.log('Hello, World!');
+
+// TASK ZJ:
+
+// Shunday function yozing, u berilgan array ichidagi
+// raqamlarni qiymatini hisoblab qaytarsin.
+
+// MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
+
+// Yuqoridagi misolda, array nested bo'lgan holdatda ham,
+// bizning function ularning yig'indisini hisoblab qaytarmoqda.
+
+function reduceNestedArray(arr: any[]): number {
+   let sum = 0;
+ 
+   for (let i = 0; i < arr.length; i++) {
+     const item = arr[i];
+ 
+     if (Array.isArray(item)) {
+       sum += reduceNestedArray(item);
+     } else if (typeof item === "number") {
+       sum += item;
+     }
+   }
+ 
+   return sum;
+ }
+ 
+ console.log(reduceNestedArray([1, [1, 2, [4]]])); 
+ console.log(reduceNestedArray([5, [3, [2, 1]], 4])); 
+ 

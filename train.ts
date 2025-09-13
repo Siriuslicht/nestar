e55d8a5b0c10d77cfdf1f6aa4e5fa3a8ba761c8a
Siriuslicht <laurairlean@gmail.com>
2025-09-13 09:16:00 +0500

    feat: MITASK-ZK

--- train.ts
+++ train.ts
@@ -1,5 +1,29 @@
 console.log('Hello, World!');
 
+
+// TASK-ZK:
+
+// Shunday function yozing, u har soniyada bir
+//  marta consolega 1 dan 5 gacha bolgan raqamlarni 
+//  chop etsin va 5 soniyadan keyin ishini toxtatsin.
+// MASALAN: printNumbers()
+
+function printNumbers(): void {
+  let count: number = 1;
+
+  const interval: NodeJS.Timeout = setInterval(() => {
+    console.log(count);
+    count++;
+
+    if (count > 5) {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+printNumbers();
+
+
 // TASK ZJ:
 
 // Shunday function yozing, u berilgan array ichidagi
@@ -10,21 +34,21 @@ console.log('Hello, World!');
 // Yuqoridagi misolda, array nested bo'lgan holdatda ham,
 // bizning function ularning yig'indisini hisoblab qaytarmoqda.
 
-function reduceNestedArray(arr: any[]): number {
-   let sum = 0;
+// function reduceNestedArray(arr: any[]): number {
+//    let sum = 0;
  
-   for (let i = 0; i < arr.length; i++) {
-     const item = arr[i];
+//    for (let i = 0; i < arr.length; i++) {
+//      const item = arr[i];
  
-     if (Array.isArray(item)) {
-       sum += reduceNestedArray(item);
-     } else if (typeof item === "number") {
-       sum += item;
-     }
-   }
+//      if (Array.isArray(item)) {
+//        sum += reduceNestedArray(item);
+//      } else if (typeof item === "number") {
+//        sum += item;
+//      }
+//    }
  
-   return sum; 
- }
+//    return sum; 
+//  }
  
- console.log(reduceNestedArray([5, [3, [2, 1]], 4])); 
+//  console.log(reduceNestedArray([5, [3, [2, 1]], 4])); 
  

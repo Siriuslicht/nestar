9891ffa4af33ca39d48904c0c408aabcb31c1ab7
Siriuslicht <laurairlean@gmail.com>
2025-09-14 14:51:26 +0500

    feat: develop signup business logic

--- apps/nestar-api/src/schemas/Member.model.ts
+++ apps/nestar-api/src/schemas/Member.model.ts
@@ -14,10 +14,10 @@ const MemberSchema = new Schema({
       default: MemberStatus.ACTIVE,
    },
 
-   MemberAuthType: {
+   memberAuthType: {
       type: String,
       enum: MemberAuthType,
-      default: MemberAuthType.PHONE,
+      default: MemberAuthType.PHONE, 
    },
 
    memberPhone: {

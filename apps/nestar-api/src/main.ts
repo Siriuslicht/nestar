dd1126e8fe7f4153a1e4f8085073c932be34c4b1
Siriuslicht <laurairlean@gmail.com>
2025-09-17 23:38:13 +0500

    fix: fixing errors

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './libs/Logging.interceptor';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor())
+
   await app.listen(process.env.PORT_API ?? 3000);
+}
 }
 bootstrap();

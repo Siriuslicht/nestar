206c724cd4267a2638bdbb20819e49e3ce3028e8
Siriuslicht <laurairlean@gmail.com>
2025-09-17 22:39:50 +0500

    feat: develop Global error GraphQL error handling error system

--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -7,6 +7,7 @@ export enum Message {
    UPLOAD_FAILED = "Upload failed!",
    BAS_REQUEST = "Bad Request!",
 
+   USED_MEMBER_NICK_OR_PHONE = "Already used member nick or phone!",
    NO_MEMBER_NICK = "No member with that membernick!",
    BLOCKED_USER = "You have been locked!",
    WRONG_PASSWORD = "Wrong password, try again!",

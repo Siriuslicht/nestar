fdcafee2c335ed1a22270a02fe38b2c71c8d01a0
Siriuslicht <laurairlean@gmail.com>
2025-09-24 18:53:23 +0500

    feat: develop getAllMembersByAdmin and updateMemberByAdmin

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,7 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { InternalServerErrorException, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
@@ -21,7 +21,7 @@ export class MemberResolver {
    public async signup(@Args('input') input: MemberInput): Promise<Member>{
          console.log("Mutation: signup");
          console.log("input:", input)
-         return this.memberService.signup(input); 
+         return await this.memberService.signup(input); 
    }
 
 
@@ -29,7 +29,7 @@ export class MemberResolver {
    public async login(@Args('input') input: LoginInput): Promise<Member>{
 
          console.log("Mutation: login");
-         return this.memberService.login(input);
+         return await this.memberService.login(input);
    }
 
 
@@ -38,7 +38,7 @@ export class MemberResolver {
    public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<String>{
       console.log("Query: checkAuth");
       console.log("memberNick:", memberNick);
-      return `Hi ${memberNick}`;
+      return await `Hi ${memberNick}`;
    }
 
 
@@ -47,7 +47,7 @@ export class MemberResolver {
    @Query(() => String)
    public async checkAuthRoles(@AuthMember() authMember: Member): Promise<String>{
       console.log("checkAuthRoles");
-      return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
+      return await `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
    }
    //Authenticated (USER, AGENT, ADMIN)
 
@@ -60,7 +60,7 @@ export class MemberResolver {
    ): Promise<Member>{
       console.log("Mutation UpdateMember");
       delete input._id
-      return this.memberService.updateMember(memberId, input);
+      return await this.memberService.updateMember(memberId, input);
    }
    
 
@@ -69,7 +69,7 @@ export class MemberResolver {
    public async getMember(@Args("memberId") input : string, @AuthMember('_id') memberId: ObjectId ): Promise<Member> {
       console.log("Query: getMember!");
       const targetId = shapeIntoMongoObjectId(input);
-      return this.memberService.getMember(memberId, targetId);
+      return await this.memberService.getMember(memberId, targetId);
    }   
 
 
@@ -79,7 +79,7 @@ export class MemberResolver {
    @Query(() => Members)
    public async getAgents(@Args("input") input : AgentsInquiry, @AuthMember('_id') memberId: ObjectId ): Promise<Members> {
       console.log("Query: getAgents");
-      return this.memberService.getAgents(memberId, input);
+      return await this.memberService.getAgents(memberId, input);
    }
 
 
@@ -90,15 +90,17 @@ export class MemberResolver {
 
    @Roles(MemberType.ADMIN)
    @UseGuards(RolesGuard)
-   @Mutation(() => String)
-   public async getAllMembersByAdmin(): Promise<string>{
+   @Mutation(() => Members)
+   public async getAllMembersByAdmin( @Args("input") input : MembersInquiry ): Promise<Members>{
       console.log("Mutation getAllMembersByAdmin!");  
-      return await this.memberService.getAllMembersByAdmin();
+      return await this.memberService.getAllMembersByAdmin(input);
+
    }
 
-
-   public async updateMemberByAdmin(): Promise<string> {
+   @Roles(MemberType.ADMIN)
+   @Mutation(() => Member)
+   public async updateMemberByAdmin(@Args("input") input: MemberUpdate): Promise<Member> {
       console.log("Mutation updateMemberByAdmin!");
-      return await this.memberService.updateMemberByAdmin();
+      return await this.memberService.updateMemberByAdmin(input);
    }
 } 
--- apps/nestar-api/src/libs/dto/member/member.input.ts
+++ apps/nestar-api/src/libs/dto/member/member.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType, Int } from "@nestjs/graphql";
 import { IsIn, IsNotEmpty, IsOptional, Length, Min } from "class-validator";
-import { MemberAuthType, MemberType } from "../../enums/member.enum";
-import { availableAgentSorts } from "../../config";
+import { MemberAuthType, MemberStatus, MemberType } from "../../enums/member.enum";
+import { availableAgentSorts, availableMemberSorts } from "../../config";
 import { Direction } from "readline";
 
 
@@ -30,6 +30,7 @@ export class MemberInput {
    memberAuthType?: MemberAuthType;
 }
 
+
 @InputType()
 export class LoginInput {
    @IsNotEmpty()
@@ -43,12 +44,14 @@ export class LoginInput {
    memberPassword: string;
 }
 
+
 @InputType()
 class AISearch {
-   @IsNotEmpty()
+   @IsOptional()
    @Field(() => String, {nullable: true})
    text?: string;
 }
+
 
 @InputType()
 export class AgentsInquiry {
@@ -76,3 +79,49 @@ export class AgentsInquiry {
    search: AISearch;
 
 }
+
+
+@InputType()
+class MISearch {
+
+   @IsOptional()
+   @Field(() => MemberStatus, {nullable: true})
+   memberStatus?: MemberStatus;
+
+   @IsOptional()
+   @Field(() => MemberType, {nullable: true})
+   memberType?: MemberType;
+
+   @IsOptional()
+   @Field(() => String, {nullable: true})
+   text?: string;
+}
+
+
+@InputType()
+export class MembersInquiry {
+   @IsNotEmpty()
+   @Min(1)
+   @Field(() => Int)
+   page: number;
+
+   @IsNotEmpty()
+   @Min(1)
+   @Field(() => Int)
+   limit: number;
+
+   @IsOptional()
+   @IsIn([availableMemberSorts])
+   @Field(() => String, {nullable: true})
+   sort?: string;
+
+   @IsOptional()
+   @Field(() => String, {nullable: true})
+   direction?: Direction;
+
+   @IsNotEmpty()
+   @Field(() => MISearch)
+   search: MISearch;
+
+}
+

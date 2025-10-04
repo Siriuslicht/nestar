9145c0dea912d4959a52866d6edd1df71b352d68
Siriuslicht <laurairlean@gmail.com>
2025-10-04 13:43:29 +0500

    fix: modify getMember business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -13,11 +13,14 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
 import { LikeService } from '../like/like.service';
+import { Followers, Followings, MeFollowed } from '../../libs/dto/follow/follow';
 
 @Injectable()
 export class MemberService  {
 
-   constructor(@InjectModel("Member") private readonly memberModel: Model<Member>,
+   constructor(
+      @InjectModel("Member") private readonly memberModel: Model<Member>,
+      @InjectModel("Follow") private readonly followModel: Model<Followers  | Followings>, 
    private authService: AuthService,
    private viewService: ViewService,   
    private likeService: LikeService,
@@ -93,10 +96,18 @@ export class MemberService  {
       //Liked ones
       const likeInput = { memberId: memberId, likeRefId: targetId, likeGroup: LikeGroup.MEMBER};
          targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
+      //meFollowed
 
-
-      //Followed ones
+      targetMember.meFollowed = await this.checkSubscription(memberId, targetId);
       return targetMember;
+   }
+
+
+   private async checkSubscription(followerId: ObjectId, followingId: ObjectId): Promise<MeFollowed[]> {
+      const result = await this.followModel.findOne({
+         followingId: followingId, followerId: followerId
+      }).exec();
+      return result ? [{ followerId: followerId, followingId: followingId, myFollowing: true}] : [ ];
    }
 
 
@@ -195,6 +206,7 @@ export class MemberService  {
 
       return result ;
    }
+
 
    public async memberStatsEditor(input: StatisticsModifier): Promise<Member> {
       console.log("executed via Property");
